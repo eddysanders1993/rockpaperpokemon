71086c232450a41e7b1218f614010dff328e96da
ees <eddysanders1993@gmail.com>
2019-01-03 20:08:28 -0500

    added ifelse for rock

--- rps_js.js
+++ rps_js.js
@@ -1,3 +1,13 @@
 var r_p_s = ['rock', 'paper', 'scissors'];
 function computerPlay(array){
     return array[Math.floor(Math.random()*array.length)];}
+function rockPaperScissors(playerSelection, computerSelection){
+if (playerSelection == "rock" && computerSelection == "paper") {
+  return "You lose! Paper beats rock.";
+} else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win! Rock beats scissors.";
+} else if (playerSelection == "rock" && computerSelection == "rock") {
+    return "Draw";
+}else if (playerSelection == "paper" && computerSelection == "rock")
+  return "You lose! Rock beats paper.";
+}
