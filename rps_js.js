6ece1ebf740566afcc53a7cfa6c05d9d409231b6
ees <eddysanders1993@gmail.com>
2019-01-04 21:07:24 -0500

    working version. counts score, plays five rounds and tells you who won

--- rps_js.js
+++ rps_js.js
@@ -1,5 +1,7 @@
-var computer_score = 1;
+var computer_score = 0;
 var player_score = 0;
+var round = 0;
+
 var r_p_s = ["rock", "paper", "scissors"];
 function computerPlay(array){
     return array[Math.floor(Math.random()*array.length)];}
@@ -7,37 +9,50 @@ function computerPlay(array){
 
 
 function game(){
-
-
-  var selection = prompt();
-  return rockPaperScissors(selection, "paper");
-
-
-
+  round += 1;
+  if (round == 5) {
+      if (computer_score > player_score) {
+    alert("Game Over. Computer wins!" + " Computer: " + computer_score + " Player: " + player_score);
+    round = 0;
+    computer_score = 0;
+    player_score = 0;
+  }
+  else if (player_score > computer_score) {
+    alert("Game Over. Player wins!" + " Computer: " + computer_score + " Player: " + player_score);
+    round = 0;
+    computer_score = 0;
+    player_score = 0;
+  }
+    else {
+      alert("Game Over. It's a tie!" + " Computer: " + computer_score + " Player: " + player_score);
+      round = 0;
+      computer_score = 0;
+      player_score = 0;
+    }
+  }
+  var selection = prompt("3, 2, 1, shoot!");
+  return rockPaperScissors(selection, computerPlay(r_p_s));
 }
 
 function rockPaperScissors(playerSelection, computerSelection){
 
 playerSelection = playerSelection.toLowerCase();
 if (playerSelection == "rock" &&  computerSelection== "paper") {
-  alert ("You lose! Paper beats rock." + " Computer: " + computer_score + " Player: " + player_score );
   computer_score += 1;
+  alert ("You lose! Paper beats rock." + " Computer: " + computer_score + " Player: " + player_score );
   return game();
-
-
 }
-//something wrong with the rest of the conditionals that is preventing game() from functioning
 else if (playerSelection == "rock" && computerSelection == "scissors") {
-
+player_score += 1;
  alert("You win! Rock beats scissors."+ " Computer: " + computer_score + " Player: " + player_score );
- return game();
+return game();
 }
   else if (playerSelection == "rock" && computerSelection == "rock") {
  alert("Draw."+ " Computer: " + computer_score + " Player: " + player_score );
- return game();
+return game();
 }
 else if (playerSelection == "paper" && computerSelection == "rock") {
-
+player_score += 1;
 alert("You win! Paper beats rock."+ " Computer: " + computer_score + " Player: " + player_score );
 return game();
 }
@@ -46,17 +61,17 @@ alert("Draw."+ " Computer: " + computer_score + " Player: " + player_score );
 return game();
 }
 else if (playerSelection == "paper" && computerSelection == "scissors") {
-
+computer_score += 1;
 alert("You lose! Scissors beats paper."+ " Computer: " + computer_score + " Player: " + player_score );
 return game();
 }
 else if (playerSelection == "scissors" && computerSelection == "rock") {
-
+computer_score += 1;
 alert("You lose! Rock beats paper."+ " Computer: " + computer_score + " Player: " + player_score );
 return game();
 }
 else if (playerSelection == "scissors" && computerSelection == "paper") {
-
+player_score += 1;
 alert("You win! Scissors beats paper."+ " Computer: " + computer_score + " Player: " + player_score );
 return game();
 }
@@ -65,4 +80,7 @@ alert("Draw."+ " Computer: " + computer_score + " Player: " + player_score );
 return game();
 }
 
+
 }
+
+game();
