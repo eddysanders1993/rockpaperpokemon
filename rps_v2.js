82fc36869779c56daf421296af8158234043a095
Edward Sanders <eddysanders1993@gmail.com>
2019-01-10 16:16:34 -0500

    works the first couple times then stops displaying results

--- rps_v2.js
+++ rps_v2.js
@@ -1,10 +1,15 @@
 var computer_score = 0;
 var player_score = 0;
 var round = 0;
-const rockwin = document.createElement('rockwin');
-const rocklose = document.createElement('rocklose');
+const win = document.createElement('win');
+const lose = document.createElement('lose');
+const draw = document.createElement('draw');
+const result = document.createElement('result');
 const body = document.querySelector('body');
-rocklose.textContent = "You lose!";
+lose.textContent = "You lose!";
+win.textContent = "You win!";
+draw.textContent = "Draw.";
+
 
 var r_p_s = ["rock", "paper", "scissors"];
 function computerPlay(array){
@@ -15,71 +20,151 @@ function computerPlay(array){
 function game(){
   round += 1;
   if (round == 5) {
+    result.style.display = "initial";
       if (computer_score > player_score) {
-    alert("Game Over. Computer wins!" + " Computer: " + computer_score + " Player: " + player_score);
-    round = 0;
-    computer_score = 0;
-    player_score = 0;
+
+        result.textContent = ("Game Over. Computer wins!" + " Computer: " + computer_score + " Player: " + player_score);
+        body.removeChild(win);
+        body.removeChild(lose);
+        body.removeChild(draw);
+        body.appendChild(result);
+
+        round = 0;
+        computer_score = 0;
+        player_score = 0;
+        result.addEventListener("click",(e) => {
+          body.removeChild(result);
+});
+
+
+
   }
   else if (player_score > computer_score) {
-    alert("Game Over. Player wins!" + " Computer: " + computer_score + " Player: " + player_score);
+    result.textContent = ("Game Over. Player wins!" + " Computer: " + computer_score + " Player: " + player_score);
     round = 0;
     computer_score = 0;
     player_score = 0;
+    body.removeChild(win);
+    body.removeChild(lose);
+    body.removeChild(draw);
+    body.appendChild(result);
+
+    result.addEventListener("click",(e) => {
+      body.removeChild(result);
+
+
+    });
+
+
   }
     else {
-      alert("Game Over. It's a tie!" + " Computer: " + computer_score + " Player: " + player_score);
+      result.textContent = ("Game Over. It's a tie!" + " Computer: " + computer_score + " Player: " + player_score);
+      body.removeChild(win);
+      body.removeChild(lose);
+      body.removeChild(draw);
+      body.appendChild(result);
+
       round = 0;
       computer_score = 0;
       player_score = 0;
+      result.addEventListener("click",(e) => {
+        body.removeChild(result);
+
+      });
+
+
+
     }
   }
 
 }
 
-function rockPaperScissors(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
 
 playerSelection = playerSelection.toLowerCase();
+
 if (playerSelection == "rock" &&  computerSelection== "paper") {
+
   computer_score += 1;
-  body.appendChild(rocklose);
+  result.style.display = "none";
+  draw.style.display = "none";
+  win.style.display = "none";
+  lose.style.display = "initial";
+  body.appendChild(lose);
+
   return game();
 }
 else if (playerSelection == "rock" && computerSelection == "scissors") {
+
 player_score += 1;
- alert("You win! Rock beats scissors."+ " Computer: " + computer_score + " Player: " + player_score );
+draw.style.display = "none";
+result.style.display = "none";
+lose.style.display = "none";
+win.style.display = "initial";
+body.appendChild(win);
+
 return game();
 }
   else if (playerSelection == "rock" && computerSelection == "rock") {
- alert("Draw."+ " Computer: " + computer_score + " Player: " + player_score );
+win.style.display = "none";
+result.style.display = "none";
+lose.style.display = "none";
+draw.style.display = "initial";
+body.appendChild(draw);
 return game();
 }
 else if (playerSelection == "paper" && computerSelection == "rock") {
 player_score += 1;
-alert("You win! Paper beats rock."+ " Computer: " + computer_score + " Player: " + player_score );
+draw.style.display = "none";
+result.style.display = "none";
+lose.style.display = "none";
+win.style.display = "initial";
+body.appendChild(win);
+
 return game();
 }
 else if (playerSelection == "paper" && computerSelection == "paper") {
-alert("Draw."+ " Computer: " + computer_score + " Player: " + player_score );
+  win.style.display = "none";
+  result.style.display = "none";
+  lose.style.display = "none";
+  draw.style.display = "initial";
+  body.appendChild(draw);
 return game();
 }
 else if (playerSelection == "paper" && computerSelection == "scissors") {
-computer_score += 1;
-alert("You lose! Scissors beats paper."+ " Computer: " + computer_score + " Player: " + player_score );
-return game();
+  computer_score += 1;
+  draw.style.display = "none";
+  win.style.display = "none";
+  lose.style.display = "initial";
+  result.style.display = "none";
+  body.appendChild(lose);
+  return game();
 }
 else if (playerSelection == "scissors" && computerSelection == "rock") {
-computer_score += 1;
-alert("You lose! Rock beats paper."+ " Computer: " + computer_score + " Player: " + player_score );
-return game();
+  computer_score += 1;
+  draw.style.display = "none";
+  result.style.display = "none";
+  win.style.display = "none";
+  lose.style.display = "initial";
+  body.appendChild(lose);
+  return game();
 }
 else if (playerSelection == "scissors" && computerSelection == "paper") {
-player_score += 1;
-alert("You win! Scissors beats paper."+ " Computer: " + computer_score + " Player: " + player_score );
+  player_score += 1;
+  draw.style.display = "none";
+  result.style.display = "none";
+  lose.style.display = "none";
+  win.style.display = "initial";
+  body.appendChild(win);
+
 return game();
 }
 else if (playerSelection == "scissors" && computerSelection == "scissors") {
-alert("Draw."+ " Computer: " + computer_score + " Player: " + player_score );
+  win.style.display = "none";
+  result.style.display = "none";
+  lose.style.display = "none";
+  draw.style.display = "initial";
+  body.appendChild(draw);
 return game();
 }
 
@@ -96,14 +181,15 @@ var rock = document.querySelector("#rock");
 var paper = document.querySelector("#paper");
 var scissors = document.querySelector("#scissors");
 
-rock.addEventListener("click",(e) => {
-    rockPaperScissors("rock", computerPlay(r_p_s));
-  });
 
-paper.addEventListener("click",(e) => {
-    rockPaperScissors("paper", computerPlay(r_p_s));
-    });
-
-scissors.addEventListener("click",(e) => {
-      rockPaperScissors("scissors", computerPlay(r_p_s));
+  rock.addEventListener("click",(e) => {
+      playRound("rock", computerPlay(r_p_s));
       });
+
+  paper.addEventListener("click",(e) => {
+      playRound("paper", computerPlay(r_p_s));
+      });
+
+  scissors.addEventListener("click",(e) => {
+        playRound("scissors", computerPlay(r_p_s));
+        });
