584631d085b3c897a3d14ae291436458bd2a5bed
Edward Sanders <eddysanders1993@gmail.com>
2019-01-11 10:18:17 -0500

    Deleted removeChild(win) (lose) (draw), allows game to function repeatedly. Before node was not found because win lose or draw was already removed in playRound so that error was allowing game() to work only a couple times. Modified html to allow an extra line for final result and text shadow for readability.

--- rps_v2.js
+++ rps_v2.js
@@ -1,76 +1,62 @@
-var computer_score = 0;
-var player_score = 0;
-var round = 0;
+let computer_score = 0;
+let player_score = 0;
+let round = 0;
 const win = document.createElement('win');
 const lose = document.createElement('lose');
 const draw = document.createElement('draw');
 const result = document.createElement('result');
 const body = document.querySelector('body');
-lose.textContent = "You lose!";
-win.textContent = "You win!";
-draw.textContent = "Draw.";
+lose.textContent = "You lose this round!";
+win.textContent = "You win this round!";
+draw.textContent = "It's a tie this round!";
 
 
-var r_p_s = ["rock", "paper", "scissors"];
+let r_p_s = ["rock", "paper", "scissors"];
 function computerPlay(array){
     return array[Math.floor(Math.random()*array.length)];}
 
 
 
 function game(){
-  round += 1;
-  if (round == 5) {
+round += 1;
+  if (round == 5){
+
     result.style.display = "initial";
       if (computer_score > player_score) {
 
-        result.textContent = ("Game Over. Computer wins!" + " Computer: " + computer_score + " Player: " + player_score);
-        body.removeChild(win);
-        body.removeChild(lose);
-        body.removeChild(draw);
-        body.appendChild(result);
+        result.textContent = ("Game Over. Computer wins!" + " Computer: " + computer_score + " Player: " + player_score + "\nPick a Pokemon to play again!" );
+
+        final.appendChild(result);
 
         round = 0;
         computer_score = 0;
         player_score = 0;
-        result.addEventListener("click",(e) => {
-          body.removeChild(result);
-});
+
+
 
 
 
   }
   else if (player_score > computer_score) {
-    result.textContent = ("Game Over. Player wins!" + " Computer: " + computer_score + " Player: " + player_score);
+    result.textContent = ("Game Over. Player wins!" + " Computer: " + computer_score + " Player: " + player_score + "\nPick a Pokemon to play again!");
     round = 0;
     computer_score = 0;
     player_score = 0;
-    body.removeChild(win);
-    body.removeChild(lose);
-    body.removeChild(draw);
-    body.appendChild(result);
 
-    result.addEventListener("click",(e) => {
-      body.removeChild(result);
-
-
-    });
-
-
+    final.appendChild(result);
   }
+
     else {
-      result.textContent = ("Game Over. It's a tie!" + " Computer: " + computer_score + " Player: " + player_score);
-      body.removeChild(win);
-      body.removeChild(lose);
-      body.removeChild(draw);
-      body.appendChild(result);
+      result.textContent = ("Game Over. It's a tie!" + " Computer: " + computer_score + " Player: " + player_score + "\nPick a Pokemon to play again!");
+
+      final.appendChild(result);
 
       round = 0;
       computer_score = 0;
       player_score = 0;
-      result.addEventListener("click",(e) => {
-        body.removeChild(result);
 
-      });
+
+
 
 
 
@@ -92,8 +78,9 @@ if (playerSelection == "rock" &&  computerSelection== "paper") {
   lose.style.display = "initial";
   body.appendChild(lose);
 
-  return game();
-}
+      return game();
+    }
+
 else if (playerSelection == "rock" && computerSelection == "scissors") {
 
 player_score += 1;
@@ -103,26 +90,33 @@ lose.style.display = "none";
 win.style.display = "initial";
 body.appendChild(win);
 
-return game();
-}
+
+    return game();
+  }
+
   else if (playerSelection == "rock" && computerSelection == "rock") {
 win.style.display = "none";
 result.style.display = "none";
 lose.style.display = "none";
 draw.style.display = "initial";
 body.appendChild(draw);
-return game();
-}
+
+    return game();
+  }
+
 else if (playerSelection == "paper" && computerSelection == "rock") {
 player_score += 1;
+
 draw.style.display = "none";
 result.style.display = "none";
 lose.style.display = "none";
 win.style.display = "initial";
 body.appendChild(win);
 
-return game();
-}
+    return game();
+  }
+
+
 else if (playerSelection == "paper" && computerSelection == "paper") {
   win.style.display = "none";
   result.style.display = "none";
